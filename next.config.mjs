854c7d9dfc999ab750e4c1f9d4b7b870a45e7d5b
with-key <rem9238@kakao.com>
2024-08-13 23:04:54 +0900

    config default settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+console.log(path.join(__dirname, "src", "sass"));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src", "sass")],
+  },
+};
 
 export default nextConfig;
